02d74bc78aac998f96c0fa12c1ca520eaaf45af6
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2021-12-30 14:13:32 -0500

    connected mysql2 to database

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 //bring dependencies
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express()
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username
+        user: 'root',
+        //your sql password
+        password: 'Suni2013!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 //routes
